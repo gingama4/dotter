3681469632375f1237947e27bc0b1d094c4c2733
gingama4 <gingama4@gmail.com>
2021-10-17 02:35:24 +0900

    add flag

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/gingama4/dotter/config"
 )
 
+var onlyExec string
+var configPath string
+
+func init() {
+	flag.StringVar(&onlyExec, "only", "", "Run specific dotfile")
+	flag.StringVar(&configPath, "path", "", "path to config")
+}
+
 func main() {
-	c := config.LoadConfig("")
+	flag.Parse()
+
+	c := config.LoadConfig(configPath)
 	fmt.Printf("%+v\n", c)
 }
