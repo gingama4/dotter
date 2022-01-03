016ab6fab505a5ecdcf3e4664ff3677572832a08
gingama4 <gingama4@gmail.com>
2022-01-03 09:40:45 +0900

    add cp

--- handler/cp.go
+++ handler/cp.go
@@ -1,8 +1,46 @@
 package handler
 
-import "github.com/gingama4/dotter/config"
+import (
+	"io"
+	"os"
+
+	"github.com/gingama4/dotter/config"
+	"github.com/gingama4/dotter/logger"
+)
 
 func Copy(s *config.Step) error {
+	es := expandPath(s.Src)
+	et := expandPath(s.Target)
+
+	if !isExists(es) {
+		logger.Log().Debug("src is not exist")
+		return nil
+	}
+
+	if isExists(et) {
+		if !s.Force {
+			return nil
+		}
+
+		os.Remove(et)
+	}
+
+	src, err := os.Open(es)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(et)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
