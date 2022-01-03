1c05ce82c62c3ddc12047e1c3cd21fcb5be01d05
gingama4 <gingama4@gmail.com>
2022-01-03 09:51:56 +0900

    add cmd

--- handler/cmd.go
+++ handler/cmd.go
@@ -1,8 +1,24 @@
 package handler
 
-import "github.com/gingama4/dotter/config"
+import (
+	"os/exec"
+	"strings"
+
+	"github.com/gingama4/dotter/config"
+)
 
 func ExecCmd(s *config.Step) error {
+
+	if s.Cmd == "" {
+		return nil
+	}
+
+	sp := strings.Split(s.Cmd, " ")
+
+	err := exec.Command(sp[0], sp[1:]...).Run()
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
